3804e83c0075ac0818bb42b7624d69144687a5cb
gitanoah <nangus2006@gmail.com>
2025-02-07 01:47:43 -0500

    Add files via upload

--- fserver.py
+++ fserver.py
@@ -1,7 +1,7 @@
 #first server using python
 #Noah Angus// From Youtube Rivaan Ranawat
 import socket
-
+import time
 
 #incase port changes bc of restraints or something to prevent hassle I split socket bind into S_H & S_P
 SERVER_HOST = "0.0.0.0"
@@ -10,7 +10,7 @@ SERVER_PORT = 7777
 #socket allow transfer of data from different inputs
 #ipv4 address
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server_socket.bind((SERVER_HOST, SERVER_PORT))
@@ -25,5 +25,17 @@ print(f'Listening on port {SERVER_PORT}...')
 
 #CLIENTE INTERACTION AND QUE (send and recieve data)
 
-client_socket, client_address = server_socket.accept()
-print(slient)
+#loop listens for multiple connection rather than 1
+#get data or http request from client to give appr response using .RECV()
+while True:  
+    client_socket, client_address = server_socket.accept()
+    req = client_socket.recv(1500).decode()
+    print(req)
+
+    #got python to return data related to client
+
+    
+##print(client_socket)
+##print(client_address)
+
+#print(slient)
